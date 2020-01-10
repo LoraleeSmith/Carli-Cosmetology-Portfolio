c9d1085b416505c02012a7c29deeda4b64902410
Loralee Dawn <50155935+LoraleeSmith@users.noreply.github.com>
2020-01-09 19:17:47 -0800

    All files
    testing, testing, 1..2..3..

---
+++ hairbycarlim.js
@@ -0,0 +1,11 @@
+$('.carousel').carousel()
+$('.carousel').carousel({
+    interval: 0005
+})
+$('#myCarousel').on('slide.bs.carousel', function () {
+    // do something...
+})
+
+$('.button').on("click"), function (event) {
+
+}
